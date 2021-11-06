0ca871cfebacde46d962fe5853e9e59cab17f0b5
Bemnet Dejene <bdejene19@gmail.com>
2021-11-06 18:22:23 -0400

    created Stock class to create stock objects to handle repetitive stock logic. Also created necessary methods such as returning stock name, ticker, percentatge move, current value, and open/close prices

---
+++ Frontend/pages/Stock.ts
@@ -0,0 +1,43 @@
+export default class Stock {
+    stockName: string;
+    ticker: string;
+    openPrice: number;
+    closePrice: number;
+    currentPrice: number;
+    percentageMove: number;
+    
+    constructor(name: string, ticker: string, open: number, close: number, currPrice: number, percentMove: number) {
+        this.stockName = name,
+        this.ticker = ticker,
+        this.openPrice= open,
+        this.closePrice = close,
+        this.currentPrice = currPrice,
+        this.percentageMove = percentMove
+    }
+
+    getStockName() {
+        return this.stockName;
+    }
+
+    getTicker() {
+        return this.ticker;
+    }
+
+    getCurrentPrice() {
+        return this.currentPrice;
+    }
+
+    getOpenPrice() {
+        return this.openPrice;
+    }
+
+    getClosePrice() {
+        return this.closePrice;
+    }
+
+    getPercentMove() {
+        let dollarMovement = this.getOpenPrice() - this.getCurrentPrice();
+        let percentMove = (dollarMovement / this.getOpenPrice()) * 100;
+        return percentMove;
+    }
+}
